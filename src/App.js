d5acc2078b90529d6998d52be6de62144d0653b3
lobitzb564 <lo.crazyred@gmail.com>
2024-04-04 19:29:58 -0400

    React boostrap addition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from'./Button.jsx';
+import Button from'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button label="Click"/>
-        
+        <Button variant="outline-secondary"> </Button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
